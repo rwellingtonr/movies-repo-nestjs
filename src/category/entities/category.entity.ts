06a54009df17b50ffc9f78981551161b8f179430
Wellington Ramos <wellington.ramos@comuniki.me>
2022-07-15 14:48:00 -0300

    (removed): removido uuid

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,5 +1,4 @@
 import { Entity, Column, CreateDateColumn, PrimaryGeneratedColumn } from "typeorm";
-import { v4 as uuid } from "uuid";
 
 @Entity("categories")
 export class Category {
@@ -14,8 +13,4 @@ export class Category {
 
 	@CreateDateColumn()
 	created_at: Date;
-
-	constructor() {
-		if (!this.id) this.id = uuid();
-	}
 }
