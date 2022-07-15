02e7906702b71e05fb40494ed23f71f5bf60953f
Wellington Ramos <wellington.ramos@comuniki.me>
2022-07-15 15:38:17 -0300

    (add): adicionado logs

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -7,27 +7,32 @@ import { UpdateMovieDto } from "./dto/update-movie.dto";
 
 @Injectable()
 export class MoviesService {
-	private logger: Logger;
-	constructor(@InjectRepository(Movies) private moviesRepository: Repository<Movies>) {
-		this.logger = new Logger(MoviesService.name);
+	private logger = new Logger(MoviesService.name);
+	constructor(@InjectRepository(Movies) private moviesRepository: Repository<Movies>) {}
+
+	private formatName(name: string) {
+		return name.toLowerCase();
 	}
 
 	async create(createMovieDto: CreateMovieDto) {
 		this.logger.log("Adding movie");
 
-		const { name } = createMovieDto;
+		const name = this.formatName(createMovieDto.name);
 
 		const alreadyExists = await this.moviesRepository.findOne({ where: { name } });
+		console.log(alreadyExists);
+
 		if (alreadyExists) {
 			throw { code: 409, message: "This movie already exists" };
 		}
 
-		const movie = this.moviesRepository.create(createMovieDto);
-
+		const movie = this.moviesRepository.create({ ...createMovieDto, name });
+		this.logger.log("Saving movie");
 		return await this.moviesRepository.save(movie);
 	}
 
 	async findAll() {
+		this.logger.log("Looking for all movies");
 		return await this.moviesRepository.find();
 	}
 
@@ -35,21 +40,23 @@ export class MoviesService {
 		const movie = await this.moviesRepository.findOne({ where: { id } });
 		if (!movie) throw new Error("Could not find this movie");
 
+		this.logger.log(`Getting movie ${movie.name}`);
 		return movie;
 	}
 
-	async update(id: string, updateMovieDto: UpdateMovieDto) {
+	async update(id: string, { description }: UpdateMovieDto) {
 		const movie = await this.moviesRepository.findOne({ where: { id } });
 		if (!movie) throw new Error("Could not find this movie");
-		const update = {
-			...movie,
-			...updateMovieDto,
-		};
 
-		return this.moviesRepository.save(update);
+		this.logger.log(`Updating description from movie ${movie.name}`);
+
+		movie.description = description;
+
+		return await this.moviesRepository.save(movie);
 	}
 
 	async remove(id: string) {
+		this.logger.log(`Removing movie id ${id}`);
 		return await this.moviesRepository.delete(id);
 	}
 }
