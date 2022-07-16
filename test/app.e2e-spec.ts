0a6c473830d90dd725714d60e5190cec2b6b66ec
Wellington Leardini <wellington-158@hotmail.com>
2022-07-15 23:16:07 -0300

    (update): iniciado testes

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,10 +2,12 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
 import * as request from "supertest";
 import { AppModule } from "./../src/app.module";
+import { randomUUID } from "crypto";
+import { CreateCategoryDto } from "src/category/dto/create-category.dto";
 
 describe("AppController (e2e)", () => {
 	let app: INestApplication;
-
+	let category: CreateCategoryDto;
 	beforeEach(async () => {
 		const moduleFixture: TestingModule = await Test.createTestingModule({
 			imports: [AppModule],
@@ -13,9 +15,25 @@ describe("AppController (e2e)", () => {
 
 		app = moduleFixture.createNestApplication();
 		await app.init();
+
+		category = {
+			description: "description",
+			name: randomUUID(),
+		};
 	});
 
-	it("/ (GET)", () => {
-		//return request(app.getHttpServer()).get("/").expect(200).expect("Hello World!");
+	it("/category (POST)", async () => {
+		return request(app.getHttpServer())
+			.post("/category")
+			.send(category)
+			.expect(201)
+			.expect("Content-Type", /json/)
+			.then((res) => {
+				category.id = res.body.id;
+				expect(res.body.name).toBe(category.name.toLowerCase());
+			});
+	});
+	it("/category", () => {
+		return request(app.getHttpServer()).get("/category").expect(200);
 	});
 });
